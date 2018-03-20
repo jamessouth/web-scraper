00b874deed256aeeb928a9d73172bd905e0afb68
jamessouth <jamesdanielsouth@gmail.com>
2018-03-19 23:21:28 -0500

    uploads

--- scraper.js
+++ scraper.js
@@ -1,1 +1,18 @@
 console.log('hello');
+const fs = require('fs');
+
+
+fs.mkdir('data', err => {
+  if(err){
+    if(err.code === 'EEXIST'){
+      console.log('directory already exists - exiting');
+      return;
+    } else {
+      console.log(err);
+      throw err;
+    }
+  } else {
+    console.log('directory created');
+    return;
+  }
+});
